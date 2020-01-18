6d04be0853af252440284db5fd04b21c3a11d987
Fabrício <fabriciovellozo@gmail.com>
2020-01-18 14:18:31 -0300

    Backend parcialmente finalizado
    Funcionalidades principais finalizadas

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const Dev = require('../models/Dev')
+const parseStringAsArray = require('../utils/parseStringAsArray')
 
 module.exports = {
     async index(request, response){
@@ -18,7 +19,7 @@ module.exports = {
     
             const { name = login, avatar_url, bio} = apiResponse.data;
         
-            const techsArray = techs.split(',').map(tech => tech.trim())
+            const techsArray = parseStringAsArray(techs);
         
             const location = {
                 type: 'Point',
@@ -37,5 +38,14 @@ module.exports = {
         }
 
         return response.json(dev)
-    }
+    },
+
+    async update(){
+
+    },
+
+    async destroy(){
+
+    },
+
 }
