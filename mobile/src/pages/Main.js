7d9c25a333501a82f2be155e833aac864a47c096
Fabrício <fabriciovellozo@gmail.com>
2020-01-27 15:01:25 -0300

    Telas finalizadas
    Parte visual implementada

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
 
-function Main() {
+function Main({ navigation }) {
     const [currentRegion, setCrurrentRegion] = useState(null);
 
     useEffect(() => {
@@ -35,19 +36,36 @@ function Main() {
     }
 
     return (
-        <MapView initialRegion={currentRegion} style={styles.map}>
-            <Marker coordinate={{ latitude: -12.6762837, longitude: -39.1581731 }}>
-                <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/2254731?s=460&v=4' }} />
-                
-                <Callout>
-                    <View style={styles.callout}>
-                        <Text style={styles.devName} >Diego Fernandes</Text>
-                        <Text style={styles.devBio}>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</Text>
-                        <Text style={styles.devTechs}>Cansado</Text>
-                    </View>
-                </Callout>
-            </Marker>
-        </MapView>
+        <>
+            <MapView initialRegion={currentRegion} style={styles.map}>
+                <Marker coordinate={{ latitude: -12.6762837, longitude: -39.1581731 }}>
+                    <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/2254731?s=460&v=4' }} />
+
+                    <Callout onPress={() => {
+                        navigation.navigate('Profile', { github_username: 'diego3g' });
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.devName} >Diego Fernandes</Text>
+                            <Text style={styles.devBio}>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</Text>
+                            <Text style={styles.devTechs}>Cansado</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
+            <View style={styles.searchForm}>
+                <TextInput
+                    style={styles.searchInput}
+                    placeholder="Buscar devs por techs..."
+                    placeholderTextColor="#999"
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                />
+
+                <TouchableOpacity onPress={() => { }} style={styles.loadButton}>
+                    <MaterialIcons name="my-location" size={20} color="#fff" />
+                </TouchableOpacity>
+            </View>
+        </>
     );
 }
 
@@ -78,9 +96,46 @@ const styles = StyleSheet.create({
         marginTop: 5,
     },
 
-    devTechs:{
-        marginTop:5,
-    }
+    devTechs: {
+        marginTop: 5,
+    },
+
+    searchForm: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row'
+    },
+
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#fff',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+        elevation: 2,
+    },
+
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8e4dff',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15,
+    },
+
 })
 
 export default Main;
