de645568cb4fa6986e29fea24921b2840769208d
Fabrício <fabriciovellozo@gmail.com>
2020-02-08 13:48:58 -0300

    Implementa Socket.io no mobile
    Comunicação e atualização e tempo real adicionadas.

--- mobile/src/services/socket.js
+++ mobile/src/services/socket.js
@@ -4,6 +4,10 @@ const socket = socketio('http://10.0.0.9:3333', {
     autoConnect: false,
 });
 
+function subscribeToNewDevs(subcribeFuncion) {
+socket.on('new-dev',subcribeFuncion);
+};
+
 function connect(latitude, longitude, techs) {
     socket.io.opts.query = {
        latitude,
@@ -12,15 +16,16 @@ function connect(latitude, longitude, techs) {
     };
     
     socket.connect();
-}
+};
 
 function disconnect() {
     if (socket.connected) {
         socket.disconnect();
     }
-}
+};
 
 export {
     connect,
     disconnect,
+    subscribeToNewDevs,
 };
