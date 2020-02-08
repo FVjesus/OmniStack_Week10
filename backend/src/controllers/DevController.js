59f5c99c9667aaa7674ae6651d1aad44ce58e6d6
Fabrício <fabriciovellozo@gmail.com>
2020-02-08 12:11:55 -0300

    Incrementa comunicação com socket.io no backend
    Adição de métodos para comunicação via socket.io.

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,9 +1,11 @@
 const axios = require('axios');
 const Dev = require('../models/Dev')
 const parseStringAsArray = require('../utils/parseStringAsArray')
+const { findConnections, sendMessage } = require('../websocket')
+
 
 module.exports = {
-    async index(request, response){
+    async index(request, response) {
         const devs = await Dev.find();
 
         return response.json(devs);
@@ -11,21 +13,21 @@ module.exports = {
 
     async store(request, response) {
         const { github_username, techs, latitude, longitude } = request.body;
-    
+
         let dev = await Dev.findOne({ github_username });
 
-        if(!dev) {
+        if (!dev) {
             const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-    
-            const { name = login, avatar_url, bio} = apiResponse.data;
-        
+
+            const { name = login, avatar_url, bio } = apiResponse.data;
+
             const techsArray = parseStringAsArray(techs);
-        
+
             const location = {
                 type: 'Point',
                 coordinates: [longitude, latitude],
             }
-        
+
             dev = await Dev.create({
                 github_username,
                 name,
@@ -35,16 +37,22 @@ module.exports = {
                 location,
             })
 
+            const sendSocketMessageTo = findConnections(
+                { latitude, longitude },
+                techsArray,
+            )
+
+            sendMessage(sendSocketMessageTo, 'new-dev', dev);
         }
 
         return response.json(dev)
     },
 
-    async update(){
+    async update() {
 
     },
 
-    async destroy(){
+    async destroy() {
 
     },
 
